5980ec1d2f8ea124b7203de0006f92863efc793c
1TSpahc <3lprxd@gmail.com>
2023-06-10 23:17:00 -0500

    documentando el codigo

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,7 +1,6 @@
 // api
 const http = require('http')
 const express = require('express')
-const cors = require('cors')
 
 // whatsapp js
 const { Client } = require('whatsapp-web.js')
@@ -19,40 +18,83 @@ const io = new Server(server, {
     origin: 'http://localhost:5173'
   }
 })
-const port = process.env.PORT || 4000
 
-// app.use(cors({
-//   origin: 'http://localhost:5173'
-// }))
+const port = process.env.PORT || 4000
 
 // Routes
 app.use(express.static(__dirname + '/public')) // eslint-disable-line
 
-const usersConnected = new Map()
+const usersConnected = new Map() // conexion de usuarios
 
+// conexion del sockte io
 io.on('connection', async (socket) => {
+  /*
+    // escuchamos el evento session para crear una nueva sesion
+    // y guardamos el socket del usuario en el mapa = {sessionId: socket}
+  */
   socket.on('session', async (sessionId) => {
+    /*
+      // en el socket solo tendremos un usuario por sesion
+      // escuchamos el evento session para crear una nueva sesion
+      // y guardamos el socket del usuario en el mapa = {sessionId: socket}
+    */
+
     if (!usersConnected.has(sessionId)) {
       usersConnected.set(sessionId, socket)
+      /*
+        // cuando un usario este conectado
+        // en la terminal aparecera el numero de usuarios conectados
+        // y el id del usuario que se conecto
+      */
 
       console.log('Número de usuarios conectados:', usersConnected.size)
       console.log('Usuario con id:', sessionId)
       console.log(`socket id ${socket.id}}`)
 
+      /*
+        // En la linea 58 creamos un evento para enviar el sessionId del usuario.
+      */
       socket.emit('socketConection', sessionId)
+
+      /*
+        // En la linea 65 usando la libreria de whatsapp-web.js le pedimos que genere un codigo QR
+        // luego lo convertimos a base64 el qr para enviarlo al cliente
+        // y creamos un evento "qrCode" para enviar el qr al cliente
+      */
 
       client.on('qr', async (qr) => {
         const qrCode = await convertQrToBase64(qr)
         socket.emit('qrCode', qrCode)
       })
 
+      /*
+        // En la linea 76 cuando la libreria de whatsapp-web.js detecte cuando el usario escanee el codigo QR
+        // si inicio sesion correctamente se ejecutara el evento "authenticated"
+        // luego creamos un evento "wpSession" para avisalr al cliente que el usuario inicio sesion correctamente
+      */
+
       client.on('authenticated', (session) => {
         socket.emit('wpSession', { session, isLogged: true })
       })
 
+      /*
+        // En la linea 86 cuando la libreria de whatsapp-web.js detecte cuando el usario cierre sesion
+        // si el usuario cierra sesion correctamente se ejecutara el evento "disconnected"
+        // luego creamos un evento "wpSessionClose" para avisalr al cliente que el usuario cerró sesion correctamente
+      */
+
       client.on('disconnected', (reason) => {
         socket.emit('wpSessionClose', { session: null, isLogged: false })
       })
+
+      /*
+        // cuando el socket escuche el evento "initFlow" cuando el cliente elija un flujo recibiremos el flujo
+        // usando la destructuracion de objetos guardamos los triggers y actions en variables
+        // escuchamos el evento "message" cuando un usuario x de whatsapp nos envie un mensaje
+        // buscamos si el mensaje del usuario coincide con algun trigger
+        // buscamos el action que coincida con el trigger
+        // enviamos el mensaje del action al usuario x de whatsapp
+      */
 
       socket.on('initFlow', async (flow) => {
         const flowContent = flow
@@ -69,11 +111,6 @@ io.on('connection', async (socket) => {
           })
 
           client.sendMessage(userMsg.from, findAction?.message)
-
-          // if (findMatch === 'errorKeyword') {
-          //   client.sendMessage(userMsg.from, 'No se reconoce la palabra clave')
-          //   return
-          // }
         })
 
         // console.log('flow iniciado')
@@ -89,7 +126,15 @@ io.on('connection', async (socket) => {
   })
 })
 
+/*
+  // Inicializamos el cliente de whatsapp
+*/
+
 client.initialize()
+
+/*
+  // Inicializamos el servidor
+*/
 
 server.listen(port, () => {
   console.log(`Server running on port ${port}`)
